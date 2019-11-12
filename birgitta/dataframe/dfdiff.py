4ee6dd01d3d15c924edc7ae8393ff3bd484db1e9
paalvibe <paalvibe@users.noreply.github.com>
2019-11-12 16:18:55 +0100

    Correct dfdiff and improve output (#39)
    Before it only checked discrepancies from expected to actual, not the opposite.
    The diff output is also improved to facilitate debugging.

--- birgitta/dataframe/dfdiff.py
+++ birgitta/dataframe/dfdiff.py
@@ -1,6 +1,8 @@
 """Functions for diffing pyspark dataframes."""
 
-__all__ = ['diff']
+import pandas as pd
+
+__all__ = ['diff', 'assert_equals']
 
 
 def array_diff(first, second):
@@ -11,17 +13,39 @@ def array_diff(first, second):
     return [item for item in first if item not in second]
 
 
-def diff(expected, actual):
-    """Diff two dataframes. However, this function has errors
-    as it only subtracts expected from actual, when it should
-    test the opposite also."""
-    # Same number of cols
+def cols_diff(expected, actual):
+    """Should have same columns"""
     if array_diff(expected.columns, actual.columns):
         expected_cols = ",".join(expected.columns)
         actual_cols = ",".join(actual.columns)
         return """Error: Cols diff
         Expected: %s
         Actual:   %s""" % (expected_cols, actual_cols)
+    return None
+
+
+def dtypes_diff(expected, actual):
+    """Should have same data types"""
+    if array_diff(expected.dtypes, actual.dtypes):
+        print("actual.dtypes:", repr(actual.dtypes))
+        expected_dtypes = repr(expected.dtypes)
+        actual_dtypes = repr(actual.dtypes)
+        return """Error: Column type diff
+        Expected: %s
+        Actual:   %s""" % (expected_dtypes, actual_dtypes)
+    return None
+
+
+def diff(expected, actual):
+    """Diff two dataframes. However, this function has errors
+    as it only subtracts expected from actual, when it should
+    test the opposite also."""
+    col_diff = cols_diff(expected, actual)
+    if col_diff:
+        return col_diff
+    dtype_diff = dtypes_diff(expected, actual)
+    if dtype_diff:
+        return dtype_diff
     expected_count = expected.count()
     actual_count = actual.count()
     # Same number of rows
@@ -30,10 +54,52 @@ def diff(expected, actual):
         Expected: %s
         Actual:   %s""" % (expected_count, actual_count)
     # Are the records the same
-    diff_df = expected.subtract(actual)
-    diff_df_count = diff_df.count()
-    if diff_df_count > 0:
-        differing_rows_str = diff_df.toPandas().to_string()
+    expected_pdf = panda_sorted(expected)
+    actual_pdf = panda_sorted(actual)
+    if not expected_pdf.equals(actual_pdf):
+        diff_df = expected.subtract(actual)
+        reverse_diff_df = actual.subtract(expected)
+        differing_rows_str = panda_sorted(diff_df).to_string()
+        reverse_differing_rows_str = panda_sorted(reverse_diff_df).to_string()
+        expected_rows_str = expected_pdf.to_string()
+        actual_rows_str = actual_pdf.to_string()
         return """Error: Rows are different
-        %s""" % (differing_rows_str)
+        Only in expected:
+        %s
+        Only in actual result:
+        %s
+        Expected:
+        %s
+        Actual:
+        %s""" % (differing_rows_str,
+                 reverse_differing_rows_str,
+                 expected_rows_str,
+                 actual_rows_str)
     return False
+
+
+def panda_assert_df_equals(expected_df, actual_df):
+    expected_pdf = panda_sorted(expected_df)
+    actual_pdf = panda_sorted(actual_df)
+    pd.testing.assert_frame_equal(expected_pdf,
+                                  actual_pdf,
+                                  check_like=True,
+                                  check_dtype=True)
+
+
+def panda_sorted(df):
+    return df.toPandas().sort_values(df.columns).reset_index(drop=True)
+
+
+def assert_equals(expected, actual):
+    # Create richer diff information
+    diff_ret = diff(expected, actual)
+    if (diff_ret):
+        cols = actual.columns
+        col_strs = []
+        for idx, col in enumerate(cols):
+            col_strs.append("%d:%s" % (idx, col))
+        print("Columns:", ", ".join(col_strs))
+        print(diff_ret)
+    # Perform a deeper comparison to catch dtype and other diffs
+    panda_assert_df_equals(expected, actual)
--- tests/dataframe/test_dfdiff.py
+++ tests/dataframe/test_dfdiff.py
@@ -77,6 +77,7 @@ def test_error_on_col_name_diff(fixtures, expected):
 def test_error_on_val_diff(fixtures, expected):
     expected_val_diff = expected.withColumn('number', F.lit(3))
     assert dfdiff.diff(fixtures, expected_val_diff) == """Error: Rows are different
+        Only in expected:
           letter  number
 0      a       1
 1      b       2
@@ -86,12 +87,75 @@ def test_error_on_val_diff(fixtures, expected):
 5      g       7
 6      h       8
 7      i       9
-8      j      10"""
+8      j      10
+        Only in actual result:
+          letter  number
+0      a       3
+1      b       3
+2      d       3
+3      e       3
+4      f       3
+5      g       3
+6      h       3
+7      i       3
+8      j       3
+        Expected:
+          letter  number
+0      a       1
+1      b       2
+2      c       3
+3      d       4
+4      e       5
+5      f       6
+6      g       7
+7      h       8
+8      i       9
+9      j      10
+        Actual:
+          letter  number
+0      a       3
+1      b       3
+2      c       3
+3      d       3
+4      e       3
+5      f       3
+6      g       3
+7      h       3
+8      i       3
+9      j       3"""
 
 
 def test_error_on_single_val_diff(fixtures, expected):
     num_when = F.when(F.col('number') == 3, 3333).otherwise(F.col('number'))
     expected_single_val_diff = expected.withColumn('number', num_when)
     assert dfdiff.diff(fixtures, expected_single_val_diff) == """Error: Rows are different
+        Only in expected:
           letter  number
-0      c       3"""
+0      c       3
+        Only in actual result:
+          letter  number
+0      c    3333
+        Expected:
+          letter  number
+0      a       1
+1      b       2
+2      c       3
+3      d       4
+4      e       5
+5      f       6
+6      g       7
+7      h       8
+8      i       9
+9      j      10
+        Actual:
+          letter  number
+0      a       1
+1      b       2
+2      c    3333
+3      d       4
+4      e       5
+5      f       6
+6      g       7
+7      h       8
+8      i       9
+9      j      10"""
