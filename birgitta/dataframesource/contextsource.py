b48840e1a483967ba24dced86b4a97ea7459ad3d
Paal de Vibe <paal@menneskemaskin.no>
2019-12-31 13:53:47 +0100

    Automatic context derivation

--- birgitta/dataframesource/contextsource.py
+++ birgitta/dataframesource/contextsource.py
@@ -3,13 +3,34 @@ from the context.
 """
 from birgitta import context
 
-
 __all__ = ['get', 'set']
 
 
 def get():
-    return context.get('BIRGITTA_DATAFRAMESOURCE')
+    source = context.get('BIRGITTA_DATAFRAMESOURCE')
+    if source:
+        return source
+    source = derive_source()
+    set(source)
+    return source
 
 
 def set(dataframe_source):
-    return context.set('BIRGITTA_DATAFRAMESOURCE', dataframe_source)
+    context.set('BIRGITTA_DATAFRAMESOURCE', dataframe_source)
+
+
+def derive_source():
+    try:
+        import dataiku
+        try:
+            # Ensure we have the actual dataiku.spark mod and not a mock
+            if (
+                    hasattr(dataiku, 'spark') and
+                    'attach_mock' not in dir(dataiku.spark)
+            ):
+                from birgitta.dataframesource.sources.dataikusource import DataikuSource  # noqa E402
+                return DataikuSource()
+        except AttributeError:
+            return None
+    except ModuleNotFoundError:
+        return None
