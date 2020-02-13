b92e79462ebf4e531c9f98121248478aefd56105
Paal de Vibe <paal@menneskemaskin.no>
2020-02-13 11:19:55 +0100

    Test correctness instead of quirk
    Test was failing since quirky bug did not appear on travis. Since we want to
    test correctness and not failures and quirks of spark, then rewrote test.

--- tests/spark/test_utc_consistency.py
+++ tests/spark/test_utc_consistency.py
@@ -1,3 +1,17 @@
+"""Test that UTC is set correctly as spark timezone, and
+that type casting to timestamp gives correct timezone.
+
+We have one record for 2016-10-30, since it has a quirky bug
+on certain spark versions/constellations.
+The quirk is that 2016-10-30 might get an extra hour, under certain
+conditions, like when using from_utc_timestamp().
+
+If spark.sql.session.timeZone is set to Europe/Oslo, this the extra hour
+is not added.
+
+When converting a date string to a timestamp, we should use a simple cast()
+instead of a trying to apply from_utc_timestamp()."""
+
 import datetime
 import pytest  # noqa F401
 from birgitta.dataframe import dfdiff
@@ -13,31 +27,26 @@ from pyspark.sql.types import StructType
 def expected(spark_session):
     expected_schema = StructType([
         StructField('f_dat', StringType()),
-        StructField('c_dat', TimestampType()),
-        StructField('f_u_dat', TimestampType())
+        StructField('c_dat', TimestampType())
     ])
     expected_data = [
         ['2016-10-30',
-         datetime.datetime(2016, 10, 30, 0, tzinfo=datetime.timezone.utc),
-         datetime.datetime(2016, 10, 30, 1, tzinfo=datetime.timezone.utc)],
+         datetime.datetime(2016, 10, 30, 0, tzinfo=datetime.timezone.utc)],
         ['2017-08-10',
-         datetime.datetime(2017, 8, 10, 0, tzinfo=datetime.timezone.utc),
          datetime.datetime(2017, 8, 10, 0, tzinfo=datetime.timezone.utc)]
     ]
     return spark_session.createDataFrame(expected_data, expected_schema)
 
 
 def test_utc_consistency(spark_session, expected):
-    """Ensure that UTC is set correctly as spark timezone, by reproducing
-    expected quirky bug of 2016-10-30 getting an extra hour.
+    """Ensure that UTC is set correctly as spark timezone, and
+    that type casting to timestamp gives correct timezone.
 
-    If spark.sql.session.timeZone is set to Europe/Oslo, this the extra hour
-    is not added.
-    This test then ensures that our default timezone options are consistent
-    and that the extra hour bug is handled correctly in type conversions.
+    When converting a date string to a timestamp, it should use
+    a simple cast().
 
-    When converting a date string to a timestamp, it should use a simple cast()
-    instead of a trying to apply from_utc_timestamp().
+    We have one record for 2016-10-30, since it has a quirky bug
+    on certain spark versions/constellations.
     """
     assert spark_session.conf.get('spark.sql.session.timeZone') == 'UTC'
 
@@ -48,13 +57,10 @@ def test_utc_consistency(spark_session, expected):
     df = spark_session.createDataFrame(rdd)
     date_df = df.withColumnRenamed("col1", "dat")
     fcol = F.col("dat")
-    f_date_format = F.date_format(fcol, 'yyyy-MM-dd')
-    f_from_utc_ts = F.from_utc_timestamp(f_date_format, 'UTC')
     ts_type = TimestampType()
     datetime_df = (
         date_df
         .withColumn('f_dat', F.date_format(fcol, 'yyyy-MM-dd'))
         .withColumn('c_dat', fcol.cast(ts_type))
-        .withColumn('f_u_dat', f_from_utc_ts)
         .drop('dat'))
     dfdiff.assert_equals(datetime_df, expected)
